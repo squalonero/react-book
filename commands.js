6f4f810f1e9d1c837a877fb9f80224338a62b867
Mirco Cuccadu <mirco.cucca@gmail.com>
2022-08-08 00:49:33 +0200

    json server init and css adj shit

---
+++ commands.js
@@ -0,0 +1,18 @@
+/**
+ * VSCODE SHORTCUTS
+ *
+ *      sfc = stateless function component
+ *
+ *
+ */
+
+/**
+ *
+ * USEFUL PACKAGES
+ *
+ *      npm install -g json-server
+ *
+ * start json server:
+ *      json-server --watch fileName.json
+ *      json-server --watch data/db.json --port 3001
+ */
